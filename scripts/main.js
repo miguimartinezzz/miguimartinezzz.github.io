739c4304c7e2f280f1771af6c0f18d0c5d9436f4
miguimartinezzz <miguel.mmontans@udc.es>
2022-08-09 13:08:03 +0200

    propper commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const mywebpage= document.querySelector('html').addEventListener('click',() =>{
+    alert('ouch,stop pocking me!');
+});
+const myHeading= document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
